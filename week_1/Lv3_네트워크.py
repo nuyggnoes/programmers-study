adab885b77d2ce2479cb3821b73978102c0b8acc
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2024-03-24 21:20:30 +0900

    lv3 네트워크(복습)

--- week_1/Lv3_네트워크.py
+++ week_1/Lv3_네트워크.py
@@ -1,3 +1,22 @@
+# def solution(n, computers):
+#     answer = 0
+#     visited = []
+
+#     def dfs(i, n, visited):
+#         for j in range(n):
+#             if j not in visited and computers[i][j] == 1:
+#                 visited.append(j)
+#                 dfs(j, n, visited)
+
+#     for i in range(n):
+#         if i not in visited:
+#             visited.append(i)
+#             answer += 1
+#             dfs(i, n, visited)
+
+#     return answer
+
+
 def solution(n, computers):
     answer = 0
     visited = []
@@ -10,10 +29,8 @@ def solution(n, computers):
 
     for i in range(n):
         if i not in visited:
-            visited.append(i)
             answer += 1
             dfs(i, n, visited)
-
     return answer
 
 
@@ -23,3 +40,4 @@ computers = [
     [1, 1, 0],
     [0, 0, 1],
 ]
+print(solution(n, computers))
